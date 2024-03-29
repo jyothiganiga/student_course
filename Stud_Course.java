f2fd9a8db9990c12bdbe53d27617c08a6d85af45
jyothi <01fe22bca226@kletech.ac.in>
2024-03-29 15:56:04 +0530

    Code for Student

---
+++ Stud_Course.java
@@ -0,0 +1,28 @@
+public class Stud_Course extends Student {
+    void course()
+    {
+        String[] subjects = {"DevOps", "Machine Learning", "C#"};
+        int[][] marks = {
+            {50, 30, 60}, 
+            {70, 45, 55}, 
+            {35, 40, 50}  
+        };
+
+        // Assuming each row represents a student and each column represents a subject
+        for (int i = 0; i < marks.length; i++) {
+            for (int j = 0; j < marks[i].length; j++) {
+                if (marks[i][j] < 40) {
+                    System.out.println("Student " + (i + 1) + " scored less than 40 in " + subjects[j]);
+                }
+            }     
+        }
+        }
+    public static void main(String args[])
+    {
+        Stud_Course sc = new Stud_Course();
+        sc.stud_det();
+        sc.course();
+        
+    }
+}
+
